817bfd16c7e05c5156b519baaf1463bfe8485c1f
malak-y <145293320+malak-y@users.noreply.github.com>
2023-11-09 22:53:37 +0200

    Add files via upload

---
+++ Waterbill.java
@@ -0,0 +1,7 @@
+public class Waterbill extends Bill{
+    private double amount;
+    public void pay() {
+        // Implementation for processing water bill payment
+        System.out.println("Water bill processed. Amount: " + amount);
+    }
+}
