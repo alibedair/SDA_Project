b88b1a7262d3fd8b4bc5f49315b4fa09edbf216f
malak-y <145293320+malak-y@users.noreply.github.com>
2023-11-09 21:56:39 +0200

    Delete Fawry.java

--- Fawry.java
+++
@@ -1,8 +0,0 @@
-public class Fawry implements WalletProviders {
-
-    @Override
-    public void transferwallet(int amount) {
-        System.out.println("Paying using Fawry "+ amount);
-    
-}
-}
