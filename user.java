cb50ed35822264ae90b88726e7d2597f865aab4e
malak-y <145293320+malak-y@users.noreply.github.com>
2023-11-09 22:47:42 +0200

    Add files via upload

---
+++ user.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class user {
+      private String password;
+      private String UserName;
+      private  String mobilenumber ;
+      private int id;
+      private List accounts;
+    
+}
